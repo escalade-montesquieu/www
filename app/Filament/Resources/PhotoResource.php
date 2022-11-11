b7ab916075ee38e6afdf4e62c12dff89224fbf00
Arthaud Proust <arthaud.proust@mink-agency.com>
2022-11-11 13:08:50 +0100

    feat: add photo resource

---
+++ app/Filament/Resources/PhotoResource.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PhotoResource\Pages;
+use App\Filament\Resources\PhotoResource\RelationManagers;
+use App\Models\Photo;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PhotoResource extends Resource
+{
+    protected static ?string $model = Photo::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-camera';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('gallery_id')
+                    ->relationship('gallery', 'name')
+                    ->required(),
+                Forms\Components\Toggle::make('pinned_homepage')
+                    ->required(),
+                Forms\Components\Textarea::make('src')
+                    ->required()
+                    ->maxLength(65535),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('gallery.name'),
+                Tables\Columns\IconColumn::make('pinned_homepage')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('src'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManagePhotos::route('/'),
+        ];
+    }
+}
