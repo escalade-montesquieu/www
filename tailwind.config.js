cfddc1b60e16238f39f52485064cb4fc43ad2d4c
arthaud.proust <arthaud.proust@mink-agency.com>
2022-11-18 21:53:37 +0100

    feat: add D.A mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,32 @@ module.exports = {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
         },
+        colors: {
+            red: {
+                light: '#FFEBEB',
+                medium: '#EF4444',
+                dark: '#991B1B',
+            },
+            blue: {
+                light: '#F0F9FF',
+                medium: '#0284C7',
+                dark: '#075985',
+            },
+            green: {
+                light: '#DCFCE7',
+                medium: '#22C55E',
+                dark: '#166534',
+            },
+            black: {
+                medium: '#686868',
+                dark: '#212529',
+            },
+            white: {
+                light: '#FEFEFE',
+                medium: '#F6F6F6',
+                dark: '#E6E6E6',
+            },
+        }
     },
 
     plugins: [require('@tailwindcss/forms')],
