6a6fef246bc701c7f9ac250505a89c8842e2618a
Arthaud <arthaudp33@gmail.com>
2022-12-20 16:26:05 +0100

    feat: gallery responsive

--- resources/views/galleries/show.blade.php
+++ resources/views/galleries/show.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
-    <article class="container flex flex-coool gap-10">
-        <x-back-link :link="route('galleries')"/>
+    <article class="container flex flex-coool gap-4 mt-10 lg:mt-32">
+        <x-back-link :link="route('galleries')" text="Retour aux galeries"/>
         <header>
             <h1 class="text-h1 mb-2">{{ $gallery->name }}</h1>
         </header>
-        <section class="grid grid-cols-2 gap-4 mt-8" id="gallery">
+        <section class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-16" id="gallery">
             @foreach($gallery->photos as $photo)
                 <a href="{{ $photo->assetSrc }}"
                    target="_blank"
