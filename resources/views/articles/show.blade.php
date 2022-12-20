a6e1586f33a9479de209a5d54807b92dae50922d
Arthaud <arthaudp33@gmail.com>
2022-12-20 16:13:18 +0100

    feat: responsive article

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <article class="container flex flex-coool gap-10">
+    <article class="container flex flex-coool gap-4 mt-10 lg:mt-32">
         <x-back-link/>
         <header>
             <h2 class="text-h2 mb-2">{{ $article->title }}</h2>
