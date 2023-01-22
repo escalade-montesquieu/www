9a789b8d3697642ae27097edb7fe49c19bad9898
arthaud-proust <arthaudp33@gmail.com>
2023-01-22 14:33:37 +0100

    feat: import events from json

--- app/Console/Commands/Import/ImportEvents.php
+++ app/Console/Commands/Import/ImportEvents.php
@@ -5,34 +5,25 @@ namespace App\Console\Commands\Import;
 use App\Models\Event;
 use App\Models\EventCategory;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Storage;
 
-class ImportEventsFromCsv extends Command
+class ImportEvents extends ImportCommand
 {
     protected $signature = 'import:events';
 
     public function handle(): int
     {
-        // Récupération du contenu du fichier CSV
-        $csvFile = Storage::get('blog_posts.csv');
+        $rows = $this->loadJson('blog_posts.json');
 
-        // Conversion du contenu en tableau
-        $csv = array_map('str_getcsv', explode("\n", $csvFile));
-
-        $header = $csv[0];
-        // Boucle sur chaque ligne du tableau (sauf la première qui contient les entêtes et la dernière qui est vide)
-        for ($i = 1, $iMax = count($csv) - 1; $i < $iMax; $i++) {
-            $data = array_combine($header, $csv[$i]);
-
-            dump($data['id']);
+        foreach ($rows as $row) {
+            dump($row['id']);
 
             Event::make([
-                'event_category_id' => EventCategory::firstWhere('slug', $data['blog'])->id,
-                'title' => $data['title'],
-                'max_places' => $data['maxplaces'] === "-1" ? NULL : $data['maxplaces'],
-                'datetime' => $data['datetime'] === 'NULL' ? NULL : $data['datetime'],
-                'location' => $data['location'] === 'NULL' ? NULL : $data['location'],
-                'description' => $data['content'] === 'NULL' ? NULL : $data['content']
+                'event_category_id' => EventCategory::firstWhere('slug', $row['blog'])->id,
+                'title' => $row['title'],
+                'max_places' => $row['maxplaces'] === "-1" ? NULL : $row['maxplaces'],
+                'datetime' => $row['datetime'],
+                'location' => $row['location'],
+                'description' => $row['content']
             ])
                 ->saveQuietly();
 
