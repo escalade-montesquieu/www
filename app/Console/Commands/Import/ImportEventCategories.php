c8b664684c57355e9f7e349cf716aeaf6f6e5d1b
arthaud-proust <arthaudp33@gmail.com>
2023-01-22 14:28:03 +0100

    feat: import event categories from json

---
+++ app/Console/Commands/Import/ImportEventCategories.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Console\Commands\Import;
+
+use App\Models\EventCategory;
+use Illuminate\Console\Command;
+
+class ImportEventCategories extends ImportCommand
+{
+    protected $signature = 'import:event-categories';
+
+    public function handle(): int
+    {
+        $rows = $this->loadJson('blog_list.json');
+
+        foreach ($rows as $row) {
+
+            dump($row['name']);
+
+            EventCategory::create([
+                'is_regular' => $row['is_regular'],
+                'title' => $row['name'],
+                'description' => $row['description'] === 'NULL' ? NULL : $row['description']
+            ]);
+        }
+
+        return Command::SUCCESS;
+    }
+}
