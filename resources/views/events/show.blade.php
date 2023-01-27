75d7585ffd73e5b5e3156a378960d687dfca21e3
Arthaud <arthaudp33@gmail.com>
2023-01-28 00:35:26 +0100

    fix: import right component

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -28,7 +28,7 @@
             @endif
         </section>
         <section class="col-span-1">
-            <livewire:events.participation :event="$event"/>
+            <livewire:events.participations :event="$event"/>
         </section>
     </section>
 
