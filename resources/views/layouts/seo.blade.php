7474c1843c40c711e2ee954bffade382360ac3dd
Arthaud <arthaudp33@gmail.com>
2023-06-11 20:07:05 +0200

    fix: url

--- resources/views/layouts/seo.blade.php
+++ resources/views/layouts/seo.blade.php
@@ -41,31 +41,38 @@
 <meta property="og:type" content="website"/>
 <meta property="og:description" content="{{ config('app.description') }}"/>
 <meta property="og:site_name" content="{{ config('app.name') }}"/>
-<meta property="og:url" content="https://escalade-montesquieu.fr"/>
+<meta property="og:url" content="{{ config('app.url') }}"/>
 <meta property="og:locale" content="fr_FR"/>
-<meta property="og:image" content="https://escalade-montesquieu.fr/assets/img/hero@0.5x.jpg?v={{ $v }}"/>
+<meta property="og:image" content="{{ config('app.url') }}/assets/img/hero@0.5x.jpg?v={{ $v }}"/>
 
 
 <!-- Twitter Meta Card -->
 <meta name="twitter:card" content="summary"/>
-<meta name="twitter:site" content="https://escalade-montesquieu.fr"/>
+<meta name="twitter:site" content="{{ config('app.url') }}"/>
 <meta name="twitter:title" content="{{ config('app.name') }}"/>
 <meta name="twitter:description" content="{{ config('app.description') }}"/>
-<meta name="twitter:image" content="https://escalade-montesquieu.fr/assets/img/hero@0.5x.jpg?v={{ $v }}">
+<meta name="twitter:image" content="{{ config('app.url') }}/assets/img/hero@0.5x.jpg?v={{ $v }}">
 
 <!-- Apple meta -->
 <meta name="apple-mobile-web-app-capable" content="yes"/>
 <meta name="apple-mobile-web-app-status-bar-style" content="#222"/>
 <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}"/>
-<link rel="apple-touch-icon" href="/assets/img/apple/apple-touch-icon.png?v={{ $v }}"/>
-<link rel="apple-touch-icon" sizes="57x57" href="/assets/img/apple/apple-touch-icon-57x57.pngv"/>
-<link rel="apple-touch-icon" sizes="72x72" href="/assets/img/apple/apple-touch-icon-72x72.png?v={{ $v }}"/>
-<link rel="apple-touch-icon" sizes="76x76" href="/assets/img/apple/apple-touch-icon-76x76.png?v={{ $v }}"/>
-<link rel="apple-touch-icon" sizes="114x114" href="/assets/img/apple/apple-touch-icon-114x114.png?v={{ $v }}"/>
-<link rel="apple-touch-icon" sizes="120x120" href="/assets/img/apple/apple-touch-icon-120x120.png?v={{ $v }}"/>
-<link rel="apple-touch-icon" sizes="144x144" href="/assets/img/apple/apple-touch-icon-144x144.png?v={{ $v }}"/>
-<link rel="apple-touch-icon" sizes="152x152" href="/assets/img/apple/apple-touch-icon-152x152.png?v={{ $v }}"/>
-<link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple/apple-touch-icon-180x180.png?v={{ $v }}"/>
+<link rel="apple-touch-icon" href="{{ config('app.url') }}/assets/img/apple/apple-touch-icon.png?v={{ $v }}"/>
+<link rel="apple-touch-icon" sizes="57x57" href="{{ config('app.url') }}/assets/img/apple/apple-touch-icon-57x57.pngv"/>
+<link rel="apple-touch-icon" sizes="72x72"
+      href="{{ config('app.url') }}/assets/img/apple/apple-touch-icon-72x72.png?v={{ $v }}"/>
+<link rel="apple-touch-icon" sizes="76x76"
+      href="{{ config('app.url') }}/assets/img/apple/apple-touch-icon-76x76.png?v={{ $v }}"/>
+<link rel="apple-touch-icon" sizes="114x114"
+      href="{{ config('app.url') }}/assets/img/apple/apple-touch-icon-114x114.png?v={{ $v }}"/>
+<link rel="apple-touch-icon" sizes="120x120"
+      href="{{ config('app.url') }}/assets/img/apple/apple-touch-icon-120x120.png?v={{ $v }}"/>
+<link rel="apple-touch-icon" sizes="144x144"
+      href="{{ config('app.url') }}/assets/img/apple/apple-touch-icon-144x144.png?v={{ $v }}"/>
+<link rel="apple-touch-icon" sizes="152x152"
+      href="{{ config('app.url') }}/assets/img/apple/apple-touch-icon-152x152.png?v={{ $v }}"/>
+<link rel="apple-touch-icon" sizes="180x180"
+      href="{{ config('app.url') }}/assets/img/apple/apple-touch-icon-180x180.png?v={{ $v }}"/>
 
-<link rel="icon" href="/assets/img/favicon.ico?v={{ $v }}">
+<link rel="icon" href="{{ config('app.url') }}/assets/img/favicon.ico?v={{ $v }}">
 <meta http-equiv="content-language" content="fr"/>
