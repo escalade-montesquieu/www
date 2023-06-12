9a53f8de1efe02c96d6375e9c3309e95bd525ecb
Arthaud <arthaudp33@gmail.com>
2023-06-12 21:41:27 +0200

    style: breadcrumb

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -5,7 +5,7 @@
             @if (!is_null($breadcrumb->url) && !$loop->last)
                 <li>
                     <a href="{{ $breadcrumb->url }}"
-                       class="breadcrumb-item underline underline-offset-4 text-blue-medium">{{ $breadcrumb->title }}</a>
+                       class="breadcrumb-item link">{{ $breadcrumb->title }}</a>
                 </li>
                 <li>/</li>
             @else
