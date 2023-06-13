cab241d4eade031b976571f101aff6785a84f789
arthaud-proust <arthaudp33@gmail.com>
2023-06-13 12:31:45 +0200

    fix: style

--- resources/views/components/galleries/card.blade.php
+++ resources/views/components/galleries/card.blade.php
@@ -1,11 +1,11 @@
-<article class="group rounded-xl flex flex-coool overflow-hidden relative">
+<article class="group rounded-xl flex flex-coool overflow-hidden relative bg-white-medium">
     @if($gallery->photo)
         <div class="aspect-5/4 overflow-hidden">
             <img class="group-hover:scale-110 duration-1000 transition-transform border-0 object-cover h-full w-full"
                  src="{{ asset('storage/'.$gallery->photo->small_image) }}">
         </div>
     @endif
-    <h3 class="text-h3 bg-white-medium p-4">
+    <h3 class="text-h3 p-4">
         <a class="link-stretch" href="{{ route('galleries.show', $gallery) }}">
             {{ $gallery->title }}
         </a>
